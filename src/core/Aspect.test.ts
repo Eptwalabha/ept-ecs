06cb1d85044a6a81d1a6b5842c5a15adfb363d2f
Eptwalabha <damiengabrielle@gmail.com>
2019-11-11 03:13:12 +0100

    rewrite tests

--- src/core/Aspect.test.ts
+++ src/core/Aspect.test.ts
@@ -1,53 +1,53 @@
 import { expect } from 'chai'
 import { Aspect } from '../index'
 
-describe ('Aspect', function () {
+describe ('Aspect', () => {
 
-    describe('all', function () {
-        it('should accepts entity if it has all the required components', function () {
-            var aspect = new Aspect().all("a", "b", "c");
-            var result = aspect.accept(["a", "b", "c"]);
+    describe('all', () => {
+        it('should accepts entity if it has all the required components', () => {
+            let aspect = new Aspect().all("a", "b", "c");
+            let result = aspect.accept(["a", "b", "c"]);
             expect(result).to.be.true;
         });
-        it('should refuses entity if it has not all the required components', function () {
-            var aspect = new Aspect().all("a", "b", "c");
-            var result = aspect.accept(["a", "c"]);
+        it('should refuses entity if it has not all the required components', () => {
+            let aspect = new Aspect().all("a", "b", "c");
+            let result = aspect.accept(["a", "c"]);
             expect(result).to.be.false;
         });
     });
 
-    describe('one', function () {
-        it('should accepts entity if it has at least one of the required components', function () {
-            var aspect = new Aspect().one("a", "b", "c");
+    describe('one', () => {
+        it('should accepts entity if it has at least one of the required components', () => {
+            let aspect = new Aspect().one("a", "b", "c");
             expect(aspect.accept(["a"])).to.be.true;
             expect(aspect.accept(["b"])).to.be.true;
             expect(aspect.accept(["c"])).to.be.true;
             expect(aspect.accept(["a", "e"])).to.be.true;
             expect(aspect.accept(["a", "b"])).to.be.true;
         });
-        it('should refuses entity if it has none of all the required components', function () {
-            var aspect = new Aspect().one("a", "b", "c");
+        it('should refuses entity if it has none of all the required components', () => {
+            let aspect = new Aspect().one("a", "b", "c");
             expect(aspect.accept([])).to.be.false;
             expect(aspect.accept(["d"])).to.be.false;
         });
     });
 
-    describe('none', function () {
-        it('should accepts entity if it has none of the excluded components', function () {
-            var aspect = new Aspect().none("a", "b", "c");
+    describe('none', () => {
+        it('should accepts entity if it has none of the excluded components', () => {
+            let aspect = new Aspect().none("a", "b", "c");
             expect(aspect.accept([])).to.be.true;
             expect(aspect.accept(["e"])).to.be.true;
         });
-        it('should refuses entity if it has any of the excluded components', function () {
-            var aspect = new Aspect().none("a", "b", "c");
+        it('should refuses entity if it has any of the excluded components', () => {
+            let aspect = new Aspect().none("a", "b", "c");
             expect(aspect.accept(["a"])).to.be.false;
             expect(aspect.accept(["a", "d"])).to.be.false;
         });
     });
 
-    describe('combinations', function () {
-        it('all and one', function () {
-            var aspect = new Aspect().all("a", "b").one("c", "d");
+    describe('combinations', () => {
+        it('all and one', () => {
+            let aspect = new Aspect().all("a", "b").one("c", "d");
             expect(aspect.accept(["a", "b"])).to.be.false;
             expect(aspect.accept(["a", "b", "c"])).to.be.true;
             expect(aspect.accept(["a", "b", "d"])).to.be.true;
@@ -56,8 +56,8 @@ describe ('Aspect', function () {
             expect(aspect.accept(["a", "b", "c", "d", "e"])).to.be.true;
         });
 
-        it('all and none', function () {
-            var aspect = new Aspect().all("a", "b").none("c", "d");
+        it('all and none', () => {
+            let aspect = new Aspect().all("a", "b").none("c", "d");
             expect(aspect.accept(["a", "b"])).to.be.true;
             expect(aspect.accept(["a", "b", "c"])).to.be.false;
             expect(aspect.accept(["a", "b", "d"])).to.be.false;
@@ -66,8 +66,8 @@ describe ('Aspect', function () {
             expect(aspect.accept(["a", "d"])).to.be.false;
         });
 
-        it('one and none', function () {
-            var aspect = new Aspect().one("a", "b").none("c", "d");
+        it('one and none', () => {
+            let aspect = new Aspect().one("a", "b").none("c", "d");
             expect(aspect.accept([])).to.be.false;
             expect(aspect.accept(["a"])).to.be.true;
             expect(aspect.accept(["b"])).to.be.true;
@@ -76,8 +76,8 @@ describe ('Aspect', function () {
             expect(aspect.accept(["c", "d"])).to.be.false;
         });
 
-        it('all, one and none', function () {
-            var aspect = new Aspect().all("a", "b").one("c", "d").none("e");
+        it('all, one and none', () => {
+            let aspect = new Aspect().all("a", "b").one("c", "d").none("e");
             expect(aspect.accept(["a", "b"])).to.be.false;
             expect(aspect.accept(["a", "b", "c"])).to.be.true;
             expect(aspect.accept(["a", "b", "d"])).to.be.true;
--- src/system/System.test.ts
+++ src/system/System.test.ts
@@ -1,22 +1,59 @@
-import { expect } from 'chai'
+import { expect, should } from 'chai'
 import { spy } from 'sinon'
 import { World, System } from '../index'
 
 class SystemTest extends System {
-    protected processSystem(): void {}
+    public call = [];
+
+    beforeProcess(): void {
+        this.call.push("beforeProcess");
+    }
+
+    protected processSystem(): void {
+        this.call.push("processSystem");
+    }
+
+    afterProcess(): void {
+        this.call.push("afterProcess");
+    }
 }
 
-describe ('System', function () {
+describe ('System', () => {
 
-    var world = new World();
-    var system = new SystemTest();
-
-    it('call the System process method on world update', function () {
+    it('call the System process method on world update', () => {
+        let world = new World();
+        let system = new SystemTest();
         world.addSystem(system);
         world.init();
-        var spyOnSystem = spy(system, "doProcessSystem");
+        let spyOnDoProcessSystem = spy(system, "doProcessSystem");
+        let spyOnProcessSystem = spy(system, "processSystem");
         world.process();
-        expect(spyOnSystem.called);
+        expect(spyOnProcessSystem.called);
+        expect(spyOnDoProcessSystem.called);
     });
 
+    it('will call processSystem only if the system is not active', () => {
+        let world = new World();
+        let system = new SystemTest();
+        let spyOnProcessSystem = spy(system, "processSystem");
+
+        world.addSystem(system)
+        system.setEnable(false);
+        world.init();
+        world.process();
+        expect(!spyOnProcessSystem.called);
+        system.setEnable(true);
+        world.process();
+        expect(spyOnProcessSystem.called);
+    });
+
+    it('will call in order beforeProcess processSystem then afterProcess when world is processed', () => {
+        let world = new World();
+        let system = new SystemTest();
+
+        world.addSystem(system)
+        world.process();
+
+        expect(system.call).to.deep.equal(["beforeProcess", "processSystem", "afterProcess"]);
+    });
 });
